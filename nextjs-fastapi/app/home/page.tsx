b690248c5e1f5a41b74042374568baf7ee210431
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2024-01-27 01:38:46 -0500

    spotify auth done

---
+++ nextjs-fastapi/app/home/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+
+export default function Home() {
+  return (
+    <main className="w-[430px] h-[932px] flex justify-center items-center bg-slate-100">
+        <h1>Home</h1>
+    </main>
+  );
+}
--- nextjs-fastapi/app/page.tsx
+++ nextjs-fastapi/app/page.tsx
@@ -1,11 +1,37 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 import { supabase } from "@/db";
+
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 //mobile screen size: 430 x 932
 
 export default function Home() {
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+  const searchParams = useSearchParams();
+
+  const router = useRouter();
+
+  async function signInWithSpotify() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "spotify",
+    });
+  }
+
+  useEffect(() => {
+    // getUser()
+
+    const urlParams = new URLSearchParams(window.location.hash.substring(1));
+    const accessToken = urlParams.get("access_token");
+    const providerAccessToken = urlParams.get("provider_token");
+    if (accessToken && providerAccessToken && router) {
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("providerAccessToken", providerAccessToken);
+      router.push("/home");
+    }
+  }, [router]);
 
   useEffect(() => {
     if (!selectedImage) return;
@@ -19,7 +45,7 @@ export default function Home() {
     //   }
     //   console.log("Uploaded file to object storage:", response);
     // });
-  }, [selectedImage])
+  }, [selectedImage]);
 
   return (
     <main className="w-[430px] h-[932px] flex justify-center items-center bg-slate-100">
@@ -27,11 +53,12 @@ export default function Home() {
         type="file"
         name="myImage"
         onChange={(event) => {
-          if (event.target.files && event.target.files[0]){
+          if (event.target.files && event.target.files[0]) {
             setSelectedImage(event.target.files[0]);
           }
         }}
       />
+      <button onClick={signInWithSpotify}>Sign in with Spotify</button>
     </main>
   );
 }
