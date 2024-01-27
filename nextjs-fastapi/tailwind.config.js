4a8b3bd2db1df4a697a7e4654cefd88f0e9bb2fa
liuwllm <liu.wllm@gmail.com>
2024-01-27 11:18:03 -0500

    Add design for album library page

--- nextjs-fastapi/tailwind.config.js
+++ nextjs-fastapi/tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      maxHeight: {
+        'carousel': '44rem'
+      }
     },
   },
   darkMode: "class",
