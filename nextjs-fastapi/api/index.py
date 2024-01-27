80b02f0649a0d1a933edb5cd3b89a89713defd45
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2024-01-27 10:53:59 -0500

    upload face works

--- nextjs-fastapi/api/index.py
+++ nextjs-fastapi/api/index.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
 # from db import supabase
 from dotenv import load_dotenv
 from typing import List
@@ -9,8 +9,8 @@ import json
 
 load_dotenv()
 
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
+# client_id = os.getenv("CLIENT_ID")
+# client_secret = os.getenv("CLIENT_SECRET")
 
 app = FastAPI()
 
@@ -20,68 +20,83 @@ app = FastAPI()
 #     print(data, count)
 #     return {"message": "Hello World"}
 
-@app.get("/api/token")
-def get_token():
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+@app.post("/api/get_profile")
+async def get_profile(request: Request):
+    body = await request.json()
+    token = body.get("token")
+    if not token:
+        raise HTTPException(status_code=400, detail="Token is missing")
 
-    url = "https://accounts.spotify.com/api/token"
+    url = "https://api.spotify.com/v1/me"
     headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Authorization": "Bearer " + token
     }
-
-    data = {"grant_type": "client_credentials"}
-    result = requests.post(url, headers=headers, data=data)
+    result = requests.get(url, headers=headers)
     json_result = json.loads(result.content)
-    token = json_result["access_token"]
-    return token
+    return json_result
 
-@app.get("/api/recommendations")
-def get_recommendations(limit: int, min_energy: int, max_energy: int, min_danceability: int, max_danceability: int, genre: str, token: str):
-    token = get_token()
-    url = "https://api.spotify.com/v1/recommendations"
-    headers = get_auth_header(token)
-    params = {
-        "limit": 1,
-        "market": ["US", "CA"],
-        "max_energy" : 0.3,
-        "min_popularity": 88,
-        "seed_genres": genre
-    }
+# @app.get("/api/token")
+# def get_token():
+#     auth_string = client_id + ":" + client_secret
+#     auth_bytes = auth_string.encode("utf-8")
+#     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
-    response = requests.get(url, headers=headers, params=params)
-    recommendations = response.json()
-    return recommendations
+#     url = "https://accounts.spotify.com/api/token"
+#     headers = {
+#         "Authorization": "Basic " + auth_base64,
+#         "Content-Type": "application/x-www-form-urlencoded"
+#     }
 
-@app.post("/api/create_playlist")
-def create_playlist(user_id: str, token: str, name: str, public: bool = True, description: str = ""):
-    url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
-    headers = get_auth_header(token)
-    payload = {
-        "name": name,
-        "public": public,
-        "description": description
-    }
+#     data = {"grant_type": "client_credentials"}
+#     result = requests.post(url, headers=headers, data=data)
+#     json_result = json.loads(result.content)
+#     token = json_result["access_token"]
+#     return token
 
-    response = requests.post(url, headers=headers, json=payload)
-    return response.json()
+# @app.get("/api/recommendations")
+# def get_recommendations(limit: int, min_energy: int, max_energy: int, min_danceability: int, max_danceability: int, genre: str, token: str):
+#     token = get_token()
+#     url = "https://api.spotify.com/v1/recommendations"
+#     headers = get_auth_header(token)
+#     params = {
+#         "limit": 1,
+#         "market": ["US", "CA"],
+#         "max_energy" : 0.3,
+#         "min_popularity": 88,
+#         "seed_genres": genre
+#     }
 
-@app.post("/api/add_tracks")
-def add_tracks_to_playlist(playlist_id: str, token: str, track_uris: List[str]):
-    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
-    headers = get_auth_header(token)
-    payload = {
-        "uris": track_uris
-    }
+#     response = requests.get(url, headers=headers, params=params)
+#     recommendations = response.json()
+#     return recommendations
 
-    response = requests.post(url, headers=headers, json=payload)
-    return response.json()
+# @app.post("/api/create_playlist")
+# def create_playlist(user_id: str, token: str, name: str, public: bool = True, description: str = ""):
+#     url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+#     headers = get_auth_header(token)
+#     payload = {
+#         "name": name,
+#         "public": public,
+#         "description": description
+#     }
 
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
+#     response = requests.post(url, headers=headers, json=payload)
+#     return response.json()
 
-token = get_token()
+# @app.post("/api/add_tracks")
+# def add_tracks_to_playlist(playlist_id: str, token: str, track_uris: List[str]):
+#     url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+#     headers = get_auth_header(token)
+#     payload = {
+#         "uris": track_uris
+#     }
 
-print(get_recommendations(1, 0.2, 1, 0.2, 1, "pop", get_token))
+#     response = requests.post(url, headers=headers, json=payload)
+#     return response.json()
+
+# def get_auth_header(token):
+#     return {"Authorization": "Bearer " + token}
+
+# token = get_token()
+
+# print(get_recommendations(1, 0.2, 1, 0.2, 1, "pop", get_token))
