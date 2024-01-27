4171325a2c35a6c58222bc983085e28ee6e86db1
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2024-01-27 02:30:04 -0500

    added write to playlist permissions

--- nextjs-fastapi/app/page.tsx
+++ nextjs-fastapi/app/page.tsx
@@ -17,6 +17,9 @@ export default function Home() {
   async function signInWithSpotify() {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "spotify",
+      options: {
+        scopes: "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public"
+      },
     });
   }
 
