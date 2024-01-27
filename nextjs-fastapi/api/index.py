babce1d28f4df3732b25db4e3a7d9cfd8d67b0e1
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2024-01-27 12:10:22 -0500

    seperate faceUpload, added album upload

--- nextjs-fastapi/api/index.py
+++ nextjs-fastapi/api/index.py
@@ -7,14 +7,14 @@ import os
 import base64
 import json
 
-from openai import OpenAI
+# from openai import OpenAI
 
 load_dotenv()
 
-client = OpenAI(api_key = os.getenv('OPENAI_API_KEY'))
+# client = OpenAI(api_key = os.getenv('OPENAI_API_KEY'))
 
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
+# client_id = os.getenv("CLIENT_ID")
+# client_secret = os.getenv("CLIENT_SECRET")
 
 app = FastAPI()
 
@@ -39,90 +39,90 @@ async def get_profile(request: Request):
     json_result = json.loads(result.content)
     return json_result
 
-@app.get("/api/recommendations")
-def get_recommendations(limit: int, min_energy: int, max_energy: int, valence: int, genre: str, track: str, token: str):
-    print("called!")
-    token = get_token()
-    url = "https://api.spotify.com/v1/recommendations"
-    headers = get_auth_header(token)
-    params = {
-        "limit": limit,
-        "min_energy": min_energy,
-        "max_energy": max_energy,
-        "valence": valence,
-        "min_popularity": 85,
-        "seed_genres": genre,
-        "seed_track": track,
-        "market": ["US", "CA"],
-    }
-
-#     url = "https://accounts.spotify.com/api/token"
-#     headers = {
-#         "Authorization": "Basic " + auth_base64,
-#         "Content-Type": "application/x-www-form-urlencoded"
+# @app.get("/api/recommendations")
+# def get_recommendations(limit: int, min_energy: int, max_energy: int, valence: int, genre: str, track: str, token: str):
+#     print("called!")
+#     token = get_token()
+#     url = "https://api.spotify.com/v1/recommendations"
+#     headers = get_auth_header(token)
+#     params = {
+#         "limit": limit,
+#         "min_energy": min_energy,
+#         "max_energy": max_energy,
+#         "valence": valence,
+#         "min_popularity": 85,
+#         "seed_genres": genre,
+#         "seed_track": track,
+#         "market": ["US", "CA"],
 #     }
 
-@app.post("/api/create_playlist")
-def create_playlist(user_id: str, playlist_name: str):
-    token = "BQBvA2Zfoqpzc5m9fK0y8kqG_J58pf7mCtk8t-W8gbqV0qzh_eZoacUqgQ1tm7xNFM9-djp3WLEIKBBIRNnZLO3nXINJdaEqeB-Ua5cPxMsNRJB5WY9boJr9pczD_zmAbKXt4wejvbKMhM18inG4NGuez-008GqVEwgtoIiJ8FQft2Em2VCGLK4k7Y1tAGCw4EctViIj4MxokZ8o0T7Ioqu6PKhwbUG5-xGyJ3qbzn2XMF-3zm14R5GvD0omKTY1jca_dmTP4Ulhnr207IA"
-    url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
-    headers = get_auth_header(token)
-    payload = {"name": playlist_name, "public": True}  # Example payload
-
-    response = requests.post(url, headers=headers, json=payload)
-
-    return response.json()
-
-#     response = requests.get(url, headers=headers, params=params)
-#     recommendations = response.json()
-#     return recommendations
+# #     url = "https://accounts.spotify.com/api/token"
+# #     headers = {
+# #         "Authorization": "Basic " + auth_base64,
+# #         "Content-Type": "application/x-www-form-urlencoded"
+# #     }
 
 # @app.post("/api/create_playlist")
-# def create_playlist(user_id: str, token: str, name: str, public: bool = True, description: str = ""):
+# def create_playlist(user_id: str, playlist_name: str):
+#     token = "BQBvA2Zfoqpzc5m9fK0y8kqG_J58pf7mCtk8t-W8gbqV0qzh_eZoacUqgQ1tm7xNFM9-djp3WLEIKBBIRNnZLO3nXINJdaEqeB-Ua5cPxMsNRJB5WY9boJr9pczD_zmAbKXt4wejvbKMhM18inG4NGuez-008GqVEwgtoIiJ8FQft2Em2VCGLK4k7Y1tAGCw4EctViIj4MxokZ8o0T7Ioqu6PKhwbUG5-xGyJ3qbzn2XMF-3zm14R5GvD0omKTY1jca_dmTP4Ulhnr207IA"
 #     url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
 #     headers = get_auth_header(token)
-#     payload = {
-#         "name": name,
-#         "public": public,
-#         "description": description
-#     }
+#     payload = {"name": playlist_name, "public": True}  # Example payload
 
 #     response = requests.post(url, headers=headers, json=payload)
+
 #     return response.json()
 
+# #     response = requests.get(url, headers=headers, params=params)
+# #     recommendations = response.json()
+# #     return recommendations
 
-@app.post("/api/vision")
-def describe_image(image_url):
-    response = client.chat.completions.create(
-    model="gpt-4-vision-preview",
-    messages=[
-        {
-        "role": "user",
-        "content": [
-            {"type": "text", "text": "Rate the image on a scale from 0.0 to 1.0 describing the positiveness conveyed by the image. The higher the more positive (e.g. happy, cheerful, euphoric), and the lower themore negative (e.g. sad, depressed, angry). Only answer with the number rating, no other words or punctuation."},
-            {
-            "type": "image_url",
-            "image_url": {
-                "url": image_url,
-            },
-            },
-        ],
-        }
-    ],
-    max_tokens=300,
-    )
+# # @app.post("/api/create_playlist")
+# # def create_playlist(user_id: str, token: str, name: str, public: bool = True, description: str = ""):
+# #     url = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+# #     headers = get_auth_header(token)
+# #     payload = {
+# #         "name": name,
+# #         "public": public,
+# #         "description": description
+# #     }
 
-    return(response.choices[0].message.content)
+# #     response = requests.post(url, headers=headers, json=payload)
+# #     return response.json()
 
 
-token = get_token()
+# @app.post("/api/vision")
+# def describe_image(image_url):
+#     response = client.chat.completions.create(
+#     model="gpt-4-vision-preview",
+#     messages=[
+#         {
+#         "role": "user",
+#         "content": [
+#             {"type": "text", "text": "Rate the image on a scale from 0.0 to 1.0 describing the positiveness conveyed by the image. The higher the more positive (e.g. happy, cheerful, euphoric), and the lower themore negative (e.g. sad, depressed, angry). Only answer with the number rating, no other words or punctuation."},
+#             {
+#             "type": "image_url",
+#             "image_url": {
+#                 "url": image_url,
+#             },
+#             },
+#         ],
+#         }
+#     ],
+#     max_tokens=300,
+#     )
 
-print(describe_image("https://media.cntraveler.com/photos/60e612ae0a709e97d73d9c60/1:1/w_3840,h_3840,c_limit/Beach%20Vacation%20Packing%20List-2021_GettyImages-1030311160.jpg"))
+#     return(response.choices[0].message.content)
 
-# print(get_recommendations(1, 0.2, 1, 0.2, 1, "pop", get_token))
 
-# print(token)
+# token = get_token()
 
-# print(create_playlist("yku0io7ib9xoq0iakf7i56zjj", "testy test"))
+# print(describe_image("https://media.cntraveler.com/photos/60e612ae0a709e97d73d9c60/1:1/w_3840,h_3840,c_limit/Beach%20Vacation%20Packing%20List-2021_GettyImages-1030311160.jpg"))
 
-# print(add_tracks_to_playlist("4ADsGrKrtqZhdvbE5YBWpB", "BQBvA2Zfoqpzc5m9fK0y8kqG_J58pf7mCtk8t-W8gbqV0qzh_eZoacUqgQ1tm7xNFM9-djp3WLEIKBBIRNnZLO3nXINJdaEqeB-Ua5cPxMsNRJB5WY9boJr9pczD_zmAbKXt4wejvbKMhM18inG4NGuez-008GqVEwgtoIiJ8FQft2Em2VCGLK4k7Y1tAGCw4EctViIj4MxokZ8o0T7Ioqu6PKhwbUG5-xGyJ3qbzn2XMF-3zm14R5GvD0omKTY1jca_dmTP4Ulhnr207IA", ["spotify:track:7CyPwkp0oE8Ro9Dd5CUDjW"]))
+# # print(get_recommendations(1, 0.2, 1, 0.2, 1, "pop", get_token))
+
+# # print(token)
+
+# # print(create_playlist("yku0io7ib9xoq0iakf7i56zjj", "testy test"))
+
+# # print(add_tracks_to_playlist("4ADsGrKrtqZhdvbE5YBWpB", "BQBvA2Zfoqpzc5m9fK0y8kqG_J58pf7mCtk8t-W8gbqV0qzh_eZoacUqgQ1tm7xNFM9-djp3WLEIKBBIRNnZLO3nXINJdaEqeB-Ua5cPxMsNRJB5WY9boJr9pczD_zmAbKXt4wejvbKMhM18inG4NGuez-008GqVEwgtoIiJ8FQft2Em2VCGLK4k7Y1tAGCw4EctViIj4MxokZ8o0T7Ioqu6PKhwbUG5-xGyJ3qbzn2XMF-3zm14R5GvD0omKTY1jca_dmTP4Ulhnr207IA", ["spotify:track:7CyPwkp0oE8Ro9Dd5CUDjW"]))
--- nextjs-fastapi/app/faceUpload/page.tsx
+++ nextjs-fastapi/app/faceUpload/page.tsx
@@ -3,55 +3,53 @@
 import { supabase } from "@/db";
 import { useEffect, useState } from "react";
 
-
 export default function Home() {
-
-    const [userProfile, setUserProfile] = useState<any>(null);
-    useEffect(() => {
-        console.log(localStorage.getItem("providerAccessToken"));
-        fetch("/api/get_profile", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-
-            },
-            body: JSON.stringify({
-                token: localStorage.getItem("providerAccessToken")
-            })
-        }).then((res) => res.json()).then((data) => {
-            console.log(data);
-            setUserProfile(data);
-        })
-    }, []);
-
+  const [userProfile, setUserProfile] = useState<any>(null);
+  useEffect(() => {
+    console.log(localStorage.getItem("providerAccessToken"));
+    fetch("/api/get_profile", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: localStorage.getItem("providerAccessToken"),
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUserProfile(data);
+      });
+  }, []);
 
   return (
     <main className="w-[430px] h-[932px] flex justify-center items-center bg-slate-100">
       <input
         type="file"
         name="faceImage"
-        onChange={(event) => {
+        onChange={async (event) => {
           if (event.target.files && event.target.files[0] && userProfile) {
-            supabase.storage.from("face_images").upload(userProfile.id, event.target.files[0], {
-              cacheControl: "3600",
-              upsert: false,
-            }).then((response) => {
-                if (response.error) {
-                    console.log(response.error);
-                    return
-                }
-                console.log("Uploaded file to object storage:", response);
-                supabase.from("users").upsert({
-                    face_image: "face_images/" + userProfile.id + ".png",
-                    spotify_username: userProfile.id,
-                }).then((response: any) => {
-                    if (response.error) {
-                        console.log(response.error);
-                        return
-                    }
-                    console.log("Updated user profile with face image");
-                })
-            })
+            const response = await supabase.storage
+              .from("face_images")
+              .upload(userProfile.id, event.target.files[0], {
+                cacheControl: "3600",
+                upsert: false,
+              });
+
+            if (response.error) {
+              console.log(response.error);
+              return;
+            }
+
+            const {data} = supabase.storage.from("face_images").getPublicUrl(
+              userProfile.id
+            );
+            await supabase.from("users").upsert({
+              face_image_path: "face_images" + "/" + userProfile.id,
+              face_image: data.publicUrl,
+              spotify_username: userProfile.id,
+            });
           }
         }}
       />
