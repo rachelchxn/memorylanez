7d25c822b9d3abee7efcf89e4ab8ca96d65e38fa
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2024-01-26 23:53:49 -0500

    basic image upload snippet complete

--- nextjs-fastapi/app/page.tsx
+++ nextjs-fastapi/app/page.tsx
@@ -1,19 +1,37 @@
 'use client'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "@/db";
 
 //mobile screen size: 430 x 932
 
 export default function Home() {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
   useEffect(() => {
-    // supabase.from("test").insert([{
-    //   name: Date.now(),
-    // }]).then((res) => {
-    //   console.log(res)
-    // })
-  }, [])
+    if (!selectedImage) return;
+    // image upload snippet
+    // supabase.storage.from("imageslol").upload("my-image.png", selectedImage, {
+    //   cacheControl: "3600",
+    //   upsert: false,
+    // }).then((response) => {
+    //   if (response.error) {
+    //     console.log(response.error);
+    //   }
+    //   console.log("Uploaded file to object storage:", response);
+    // });
+  }, [selectedImage])
+
   return (
     <main className="w-[430px] h-[932px] flex justify-center items-center bg-slate-100">
+      <input
+        type="file"
+        name="myImage"
+        onChange={(event) => {
+          if (event.target.files && event.target.files[0]){
+            setSelectedImage(event.target.files[0]);
+          }
+        }}
+      />
     </main>
   );
 }
