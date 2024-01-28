59ab942f3bc5eb23dcaf13197dcf42ca54d72ec7
liuwllm <liu.wllm@gmail.com>
2024-01-28 06:46:43 -0500

    AM I DONE PLS

--- nextjs-fastapi/app/messages/page.tsx
+++ nextjs-fastapi/app/messages/page.tsx
@@ -77,38 +77,6 @@ export default function Library() {
 
   }, [userProfile, setFaceImageUrl])
 
-  useEffect(() => {
-    if (userProfile && userProfile.id) {
-      getAlbums(userProfile.id);
-    }
-  }, [userProfile]); // useEffect depends on userProfile
-
-  const getAlbums = async (userID: string) => {
-    try {
-      // Query the 'albums' table
-      let query = supabase.from("albums").select("*").eq("owner", userID);
-      // Executing the query
-      const { data, error } = await query;
-
-      if (error) {
-        throw error;
-      }
-
-      if (data) {
-        // Filter albums with non-empty images array
-        const filteredAlbums = data.filter(
-          (album) => album.images && album.images.length > 0
-        );
-        setAlbums(filteredAlbums);
-        console.log(data);
-      }
-    } catch (error) {
-      console.error("Error fetching albums:", error);
-    }
-  };
-
-  console.log(fromUserProfile, faceImageUrl, inviteData)
-
   return (
     <main className="flex justify-center bg-bgbeige min-h-screen">
       <div className="relative max-w-lg flex-col justify-center w-full h-screen bg-photoalbum px-10 py-10 overflow-hidden">
@@ -120,26 +88,20 @@ export default function Library() {
               </div>
             </div>
           </div>
-          <ScrollShadow
-            hideScrollBar
-            className="relative max-h-carousel w-full"
-          >
-            {albums.map((album) => (
-              <Link key={album.id} href={`/albums/${album.id}`}>
+              <Link href={inviteData.link}>
                 <div
-                  key={album}
                   className="w-[100%] p-4 flex gap-4 mb-4 rounded-lg"
                   style={{ backgroundColor: "rgba(245, 245, 220, 0.5)" }}
                 >
                   <img
-                    src={album.images[0]}
+                    src={faceImageUrl}
                     className="w-24 h-24 object-cover rounded-sm"
                   />
-                  <h2 className="text-black">{album.title}</h2>
+                  <p className="text-burnt">
+                    {inviteData.message}
+                  </p>
                 </div>
               </Link>
-            ))}
-          </ScrollShadow>
         </div>
         <div className="-m-[64rem] -z-20">
           <Image width={2000} src={orange} alt="" />
