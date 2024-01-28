7f56d287fe45b6e7ff44b1163c15c46f893bcf81
Rachel Chen <85042944+rachelchxn@users.noreply.github.com>
2024-01-28 03:16:19 -0500

    fix album

--- nextjs-fastapi/app/albums/[albumId]/page.tsx
+++ nextjs-fastapi/app/albums/[albumId]/page.tsx
@@ -12,8 +12,8 @@ import orange from "../../../public/circle.png";
 import pink from "../../../public/ROSE.png";
 import Image from "next/image";
 import { Button } from "@nextui-org/react";
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 interface Track {
   id: string;
@@ -22,14 +22,12 @@ interface Track {
 
 export default function Album({ params }: { params: { albumId: string } }) {
   const [userProfile, setUserProfile] = useState<any>(null);
-<<<<<<< HEAD
   const [tracks, setTracks] = useState<Track[]>([]);
   const [count, setCount] = useState<number>(0);
   const [currPhoto, setCurrPhoto] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
   const [currTrack, setCurrTrack] = useState<Track>();
-  let albumLength = 0;
-=======
->>>>>>> 1a0eb15e8c1d5c9742349db5eb6cc74035b4abb2
+  const [albumLength, setAlbumLength] = useState<number>();
 
   // const router = useRouter();
   useEffect(() => {
@@ -54,8 +52,6 @@ export default function Album({ params }: { params: { albumId: string } }) {
 
   console.log(slug);
 
-  const [tracks, setTracks] = useState<Track[]>([]);
-
   useEffect(() => {
     async function getTracks() {
       const { data, error } = await supabase
@@ -72,78 +68,92 @@ export default function Album({ params }: { params: { albumId: string } }) {
       setTracks(data.tracks);
     }
 
-    getTracks()
-  }, [params.albumId])
+    getTracks();
+  }, [params.albumId]);
 
   useEffect(() => {
-    async function updateSlideshow() {
-      const { data, error } = await supabase
-        .from("albums")
-        .select("tracks, images")
-        .eq("id", params.albumId) 
-        .single();
-      if (!data) {
-        console.log(error);
-        return;
-      }
-      if (count >= 0 && count <= data.tracks.length){
-        setCurrPhoto(data.images[count]);
-        setCurrTrack(data.tracks[count]);
-      }
-      albumLength = data.tracks.length;
-    }
-  }, [count])
+    console.log(count);
+    updateSlideshow();
+  }, [count]);
 
-  console.log(params.albumId)
+  async function updateSlideshow() {
+    const { data, error } = await supabase
+      .from("albums")
+      .select("tracks, images, title")
+      .eq("id", params.albumId)
+      .single();
+    if (!data) {
+      console.log(error);
+      return;
+    }
+    if (count >= 0 && count <= data.tracks.length) {
+      setCurrPhoto(data.images[count]);
+      setCurrTrack(data.tracks[count]);
+    }
+
+    setAlbumLength(data.tracks.length);
+    setTitle(data.title);
+  }
+
+  console.log(params.albumId);
 
   return (
     <main className="flex justify-center bg-bgbeige min-h-screen">
-      <div className="relative max-w-lg flex-col w-full h-screen bg-photoalbum px-10 py-[24rem] overflow-hidden">
+      <div className="relative max-w-lg flex-col w-full h-screen bg-photoalbum px-10 py-10 overflow-hidden">
         <div className="relative z-10">
+          <h1>{title}</h1>
           <div className="flex justify-center">
-            <div className="w-full h-auto">
-              <Image src={currPhoto} alt="" />
-            </div>
+            <img src={currPhoto} alt="" className="w-[100%] bottom-[auto]" />
           </div>
         </div>
         <div className="relative z-10">
-            <div className="fixed bottom-0">
-              <div className="flex justify-center">
-                {(count == 0) ?
-                (<div className="mt-16 mx-3">
+          <div className="fixed bottom-10">
+            <div className="flex justify-center">
+              {count == 0 ? (
+                <div className="mt-16 mx-3">
                   <Button isIconOnly className="bg-burnt rounded-md">
                     <ArrowBackIosNewIcon className="text-gray-500" />
                   </Button>
-                </div>):
-                (<div className="mt-16 mx-3">
-                  <Button onClick={() => setCount(count - 1)} isIconOnly className="bg-burnt rounded-md">
-                    <ArrowBackIosNewIcon className="text-gray-500" />
-                  </Button>
-                </div>)}
-                <div className="mt-10 mx-3">
-                  <iframe
-                    src={`https://open.spotify.com/embed/track/${currTrack}`}
-                    allow="encrypted-media"
-                    height="80"
-                    width="300"
-                    className="rounded-xl"
-                  >
-                  </iframe>
                 </div>
-                {(count == albumLength) ?
-                (<div className="mt-16">
-                  <Button isIconOnly className="bg-burnt rounded-md">
-                    <ArrowForwardIosIcon className="text-gray-500" />
+              ) : (
+                <div className="mt-16 mx-3">
+                  <Button
+                    onClick={() => setCount(count - 1)}
+                    isIconOnly
+                    className="bg-burnt rounded-md"
+                  >
+                    <ArrowBackIosNewIcon className="text-gray-500" />
                   </Button>
-                </div>):
-                (<div className="mt-16">
-                  <Button onClick={() => setCount(count + 1)} isIconOnly className="bg-burnt rounded-md">
+                </div>
+              )}
+              <div className="mt-10 mx-3">
+                <iframe
+                  src={`https://open.spotify.com/embed/track/${currTrack}`}
+                  allow="encrypted-media"
+                  height="80"
+                  width="300"
+                  className="rounded-xl"
+                ></iframe>
+              </div>
+              {count == albumLength ? (
+                <div className="mt-16">
+                  <Button isIconOnly className="bg-burnt rounded-md">
                     <ArrowForwardIosIcon className="text-white" />
                   </Button>
-                </div>)}
-                
-              </div>
+                </div>
+              ) : (
+                <div className="mt-16">
+                  <Button
+                    onClick={() => setCount(count + 1)}
+                    isIconOnly
+                    className="bg-burnt rounded-md"
+                  >
+                    <ArrowForwardIosIcon className="text-white" />
+                  </Button>
+                </div>
+              )}
             </div>
+          </div>
         </div>
         <div className="-m-[32rem] -z-20">
           <Image width={2000} src={orange} alt="" />
